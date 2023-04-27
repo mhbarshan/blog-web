48dc07d882854f63d8bff833c0c984bedb962da2
mhbarshan <64357462+mhbarshan@users.noreply.github.com>
2023-04-27 17:30:13 +0600

    Delete client directory

--- client/src/components/PrivateRoute.js
+++
@@ -1,7 +0,0 @@
-import { Navigate } from "react-router-dom";
-import { useAuth } from "../context/authContext";
-
-export default function PrivateRoute({ children }) {
-  const { currentUser } = useAuth();
-  return currentUser ? children : <Navigate to="/login" />;
-}
